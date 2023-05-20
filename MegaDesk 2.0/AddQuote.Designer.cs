8708cd56dd17a24efe026a6aad6b55f9b87c0283
Dallin Olson <86103274+DallinO@users.noreply.github.com>
2023-05-20 12:00:56 -0600

    Minor Design Fix

--- MegaDesk 2.0/AddQuote.Designer.cs
+++ MegaDesk 2.0/AddQuote.Designer.cs
@@ -59,7 +59,7 @@ namespace MegaDesk_Muzo
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label1.Location = new System.Drawing.Point(171, 16);
+            this.label1.Location = new System.Drawing.Point(227, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(137, 34);
@@ -72,7 +72,7 @@ namespace MegaDesk_Muzo
             this.label2.Location = new System.Drawing.Point(120, 83);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 28);
+            this.label2.Size = new System.Drawing.Size(164, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Customer Name:";
             // 
@@ -230,7 +230,7 @@ namespace MegaDesk_Muzo
             this.getquotebutton.Name = "getquotebutton";
             this.getquotebutton.Size = new System.Drawing.Size(150, 38);
             this.getquotebutton.TabIndex = 9;
-            this.getquotebutton.Text = "Get Quote";
+            this.getquotebutton.Text = "Create Quote";
             this.getquotebutton.UseVisualStyleBackColor = true;
             this.getquotebutton.Click += new System.EventHandler(this.getquotebutton_Click_1);
             // 
